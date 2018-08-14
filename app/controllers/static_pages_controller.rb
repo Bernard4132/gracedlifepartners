b96b93426409c7782a6c10a53ce341949d7b32f6
bernard4132 <bernardlomo2@gmail.com>
2018-08-14 21:58:29 +0000

    first push

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,35 @@
+class StaticPagesController < ApplicationController
+
+def home
+	
+end
+
+def aboutus
+	
+end
+
+def fulltime
+	
+end
+
+def parttime
+	
+end
+
+def volunteers
+	
+end
+
+def onetimedonorhere
+	
+end
+
+def monthlydonorhere
+	
+end
+
+def yearlydonorhere
+	
+end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#home"
+  match 'aboutus', to: 'static_pages#aboutus', via: 'get'
+  match 'fulltime', to: 'static_pages#fulltime', via: 'get'
+  match 'parttime', to: 'static_pages#parttime', via: 'get'
+  match 'volunteers', to: 'static_pages#volunteers', via: 'get'
+  match 'onetimedonorhere', to: 'static_pages#onetimedonorhere', via: 'get'
+  match 'monthlydonorhere', to: 'static_pages#monthlydonorhere', via: 'get'
+  match 'yearlydonorhere', to: 'static_pages#yearlydonorhere', via: 'get'
+end
